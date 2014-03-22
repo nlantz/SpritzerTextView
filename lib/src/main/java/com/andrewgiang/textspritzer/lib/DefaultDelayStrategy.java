b036a4b54eb1ef5f3a390ff5acf826ada242acdf
nlantz <nicholaslantz@gmail.com>
2014-03-22 14:49:52 -0700

    Update DefaultDelayStrategy.java
    Implemented less aggressive pausing

--- lib/src/main/java/com/andrewgiang/textspritzer/lib/DefaultDelayStrategy.java
+++ lib/src/main/java/com/andrewgiang/textspritzer/lib/DefaultDelayStrategy.java
@@ -2,12 +2,15 @@ package com.andrewgiang.textspritzer.lib;
 
 /**
  * Created by andrewgiang on 3/19/14.
+ * 
+ * 
+ * Edited by nlantz on 3/22/14
  */
 public class DefaultDelayStrategy implements DelayStrategy {
     @Override
     public int delayMultiplier(String word) {
-        if (word.length() >= 6 || word.contains(",") || word.contains(":") || word.contains(";") || word.contains(".") || word.contains("?") || word.contains("!") || word.contains("\"")) {
-            return 3;
+        if (word.length() >= 13 || word.contains(".") || word.contains("?") || word.contains("!") {
+            return 2;
         }
         return 1;
     }
